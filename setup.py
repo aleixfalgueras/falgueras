b9af78de9c2b3c51802c41fa1ac6a6e61ce89c87
aleixfalgueras <feina.aleix@gmail.com>
2025-01-04 13:24:22 -0500

    Action publish-to-pypi.yml added.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 
-# function to read dependencies from requirements.txt
 def parse_requirements(filename):
+    """Function to read dependencies from requirements.txt"""
     with open(filename, 'r') as f:
         return [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
@@ -17,7 +17,23 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/aleixfalgueras/falgueras',
     packages=find_packages(),
-    install_requires=parse_requirements('requirements.txt'),  # Dynamically load dependencies
+    install_requires=[
+        "colorama~=0.4.6",
+        "pytz~=2024.1",
+        "pandas~=2.2.2",
+        "db-dtypes~=1.3.1",
+        "numpy~=2.2.1",
+        "google-api-core~=2.24.0",
+        "google-auth~=2.37.0",
+        "google-cloud-bigquery~=3.27.0",
+        "google-cloud-bigquery-storage~=2.27.0",
+        "google-cloud-storage~=2.19.0",
+        "google-api-python-client~=2.156.0",
+        "google-cloud-secret-manager~=2.22.0",
+        "google-cloud-language~=2.16.0",
+        "protobuf~=5.29.2",
+        "requests~=2.32.3"
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
