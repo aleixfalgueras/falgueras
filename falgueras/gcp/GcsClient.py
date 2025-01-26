a17cc4fb93ab23b79ba70fe3a41bcc0db7606675
aleixfalgueras <feina.aleix@gmail.com>
2025-01-26 12:50:08 -0500

    add logging to GCS + content_encoding for writing string data

--- falgueras/gcp/GcsClient.py
+++ falgueras/gcp/GcsClient.py
@@ -2,6 +2,10 @@ from typing import Optional
 
 from google.cloud import storage
 
+from falgueras.common.logging_utils import get_colored_logger
+
+logger = get_colored_logger(__name__)
+
 
 class GcsClient:
     """A class to handle Google Cloud Storage (GCS) operations."""
@@ -27,6 +31,7 @@ class GcsClient:
             return blob.download_as_text()
 
         except Exception as e:
+            logger.error(e)
             raise RuntimeError(f"Failed to read {path} from {bucket}: {e}")
 
     def read_bytes(self, bucket_name: str, filename: str) -> bytes:
@@ -38,9 +43,15 @@ class GcsClient:
             return blob.download_as_bytes()
 
         except Exception as e:
+            logger.error(e)
             raise Exception(f"Error reading PDF from GCS: {str(e)}")
 
-    def write_string(self, bucket_name: str, path: str, data: str, content_type: str = "text/csv") -> str:
+    def write_string(self,
+                     bucket_name: str,
+                     path: str,
+                     data: str,
+                     content_type: str = "text/csv",
+                     content_encoding: Optional[str] = "utf-8") -> str:
         """
         Writes a string to a Google Cloud Storage bucket.
 
@@ -49,6 +60,7 @@ class GcsClient:
             path (str): The path where the file should be written within the bucket.
             data (str): The string data to be written.
             content_type (str): The MIME type of the content.
+            content_encoding (Optional[str]): Content encoding of the file
 
         Raises: Return public url of the written object.
         """
@@ -57,11 +69,16 @@ class GcsClient:
 
         try:
             blob = self.client.get_bucket(bucket_name).blob(path)
+            blob.content_encoding = content_encoding
+
+            logger.info(f"Writing string data file (type: {content_type}, encoding: {content_encoding}) in "
+                        f"gs://{bucket_name}/{path}")
             blob.upload_from_string(data, content_type=content_type)
 
             return blob.public_url
 
         except Exception as e:
+            logger.error(e)
             raise RuntimeError(f"Failed to write data to {path} in {bucket_name}: {e}")
 
     def exists_object(self, filename: str, bucket_name: str) -> bool:
@@ -69,8 +86,9 @@ class GcsClient:
         try:
             bucket = self.client.bucket(bucket_name)
             blob = bucket.blob(filename)
-            
+
             return blob.exists()
-        
+
         except Exception as e:
+            logger.error(e)
             raise RuntimeError(f"Failed to check existence of {filename} in bucket {bucket_name}: {e}")
